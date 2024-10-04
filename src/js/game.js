22f88d97701bf43c5697a1c696f32af6dcd955bb
simplespacej <my-ptogramming@mail.ru>
2024-10-04 19:58:35 +0300

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,20 @@
+class Game {
+    start() {
+        console.log('game started');
+    }
+}
+
+class GameSavingData {
+    // Логика сохранения игры
+}
+
+function readGameSaving() {
+    // Логика чтения сохранения игры
+}
+
+function writeGameSaving() {
+    // Логика записи сохранения игры
+}
+
+export default Game;
+export { GameSavingData, readGameSaving, writeGameSaving };
